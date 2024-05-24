7ad2ace2a05eedb6d77bb6b2c06cc3107f5bf679
arman-rai <118722334+arman-rai@users.noreply.github.com>
2024-05-24 22:11:45 +0545

    Update cppModel.cpp

---
+++ cppModel.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+// code file is compiled to form a binary executable 
+
+// program memory model 
+// binary file is booted in RAM and is run statement by statement
+// variables are allocated memory address in seperate addresses than functions
+// the function return memory address is allocated in the CPU  
